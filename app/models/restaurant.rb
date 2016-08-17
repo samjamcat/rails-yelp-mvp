0d682fdeab21d30293cf3e6e443d27186865173d
sam cathcart <sc@10past12.co>
2016-08-17 16:09:24 +0100

    change category

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,8 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
+
+  CATEGORIES = %w(Chinese Italian Japanese French Belgian)
+
+  validates :category, inclusion: { in: CATEGORIES }
 end
